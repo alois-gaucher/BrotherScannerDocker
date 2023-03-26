4bf8b542328a7b2f4f8e5a12a93eb37794f4e71b
Aloïs GAUCHER <agaucher@eutech.fr>
2023-03-26 14:26:34 +0200

    fix sleeping infinite

--- files/runScanner.sh
+++ files/runScanner.sh
@@ -11,15 +11,13 @@ env > /opt/brother/scanner/env.txt
 su - $USERNAME -c "/usr/bin/brsaneconfig4 -a name=$NAME model=$MODEL ip=$IPADDRESS"
 su - $USERNAME -c "/usr/bin/brscan-skey"
 
-while true;
-do
-  sleep 1000
-done
+sleep 10000
 
 echo "Replacing scripts"
 
-su - $USERNAME -c "rm -rf /opt/brother/scanner/brscan-skey/script"
-su - $USERNAME -c "cp /script/* /opt/brother/scanner/brscan-skey/script"
+rm -rf /opt/brother/scanner/brscan-skey/script
+cp /script/* /opt/brother/scanner/brscan-skey/script
+chmod -R 777 /opt/brother/scanner/brscan-skey/script
 
 echo "Startup finished"
 
