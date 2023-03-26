849d812b4ccce1dfebe0bdcd7a1374651c924323
Aloïs GAUCHER <agaucher@eutech.fr>
2023-03-26 14:40:32 +0200

    fix sleep

--- files/runScanner.sh
+++ files/runScanner.sh
@@ -11,7 +11,7 @@ env > /opt/brother/scanner/env.txt
 su - $USERNAME -c "/usr/bin/brsaneconfig4 -a name=$NAME model=$MODEL ip=$IPADDRESS"
 su - $USERNAME -c "/usr/bin/brscan-skey"
 
-sleep 10000
+sleep 30
 
 echo "Replacing scripts"
 
